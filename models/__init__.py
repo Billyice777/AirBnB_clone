0c2b879d5a3cd1f9ba86700c84d223a649660fe9
Billyice777 <uwemedimoekpewohp@gmail.com>
2023-10-17 06:06:27 -0700

    0x00. AirBnB clone - The console

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,5 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """ Creates a unique FileStorage instance for application """
-
 from models.engine.file_storage import FileStorage
-
-
 storage = FileStorage()
 storage.reload()
